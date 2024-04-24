98090678d35093cd9d0207efa939e380a2ad2830
Fran Bec <franbecvort@gmail.com>
2024-04-24 21:33:47 +0100

    changed color pallete, added logo

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -69,9 +69,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <Link href="/">
           <Image
-            width={176}
-            height={32}
-            src={"/images/logo/logo.svg"}
+            width={242}
+            height={134}
+            src={"/images/logo.png"}
             alt="Logo"
             priority
           />
