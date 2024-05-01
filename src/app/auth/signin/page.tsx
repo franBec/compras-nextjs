94d62c594b74b0dd85c1c7fd2bdc3231edf5a122
Fran Bec <franbecvort@gmail.com>
2024-05-01 12:39:18 +0100

    protectedRoutes.ts

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
---
+++ src/utils/protectedRoutes.ts
@@ -0,0 +1,55 @@
+const protectedRoutes = [
+    {
+        "url": "/coordinaciones",
+        "permiso": "COMPRAS_PG_COO_MENU"
+    },
+    {
+        "url": "/expedientes",
+        "permiso": "COMPRAS_CE_MENU"
+    },
+    {
+        "url": "/expedientes-ue",
+        "permiso": "COMPRAS_CE_UE_EXPEDIENTE_MENU"
+    },
+    {
+        "url": "/productos",
+        "permiso": "COMPRAS_PG_PD_MENU"
+    },
+    {
+        "url": "/proveedores",
+        "permiso": "COMPRAS_PV_MENU"
+    },
+    {
+        "url": "/requisitos",
+        "permiso": "COMPRAS_PG_RR_MENU"
+    },
+    {
+        "url": "/rubros",
+        "permiso": "COMPRAS_PG_RUBRO_MENU"
+    },
+    {
+        "url": "/solicitar-producto",
+        "permiso": "COMPRAS_PG_PD_UE_MENU"
+    },
+    {
+        "url": "/tipo-asignacion",
+        "permiso": "COMPRAS_PG_TA_MENU"
+    },
+    {
+        "url": "/tipo-expediente-compra",
+        "permiso": "COMPRAS_PG_TEC_MENU"
+    },
+    {
+        "url": "/tipo-contratacion",
+        "permiso": "COMPRAS_PG_TCON_MENU"
+    },
+    {
+        "url": "/unidad-ejecutora",
+        "permiso": "COMPRAS_PG_UE_MENU"
+    },
+    {
+        "url": "/unidad-medida",
+        "permiso": "COMPRAS_PG_UM_MENU"
+    },
+]
+export default protectedRoutes;
